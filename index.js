78ae897c9311de610a83d20b9b543e1d10166ede
Kelvina <Kelvina-web.com>
2025-03-05 11:17:18 +0300

    Adding files

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const body = document.querySelector('body');
+        const newId = document.createElement('p');
+        newId.textContent = data.id;
+        body.appendChild(newId);
+    })
+    .catch(error => {
+        const body = document.querySelector('body');
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = error.message;
+        body.appendChild(errorMessage);
+    });
+}
